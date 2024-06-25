51b522528315fdd1c825ee1834d59c58c5629cc5
bayarchimegkalina <bayarchimegkalina@edu.dju.ac.kr>
2024-06-25 14:52:33 +0900

    Adds keycloak settings to the Spring Boot application.

--- src/main/java/com/easycoding/myjava/controller/AccountController.java
+++ src/main/java/com/easycoding/myjava/controller/AccountController.java
@@ -3,17 +3,17 @@ package com.easycoding.myjava.controller;
 import com.easycoding.myjava.common.json.JsonResult;
 import com.easycoding.myjava.entity.account.AccountVo;
 import com.easycoding.myjava.service.AccountService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
+@Tag(name = "Account")
 public class AccountController {
     @Autowired
     AccountService accountService;
@@ -24,6 +24,7 @@ public class AccountController {
         return accountList;
     }
     @RequestMapping(value="/account/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public JsonResult createNewAccount(@RequestBody AccountVo accountVo, HttpServletRequest request) throws Exception{
         return  accountService.createNewAccount(accountVo, request);
     }
